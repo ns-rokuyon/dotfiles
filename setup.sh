e94aacc9aa1863773dc697eda194b62fcdeb704f
ns-rokuyon <ness3psi@gmail.com>
2015-11-14 16:06:18 +0900

    add cygwin setup

--- setup.sh
+++ setup.sh
@@ -2,6 +2,15 @@
 
 DIR=$(cd $(dirname $0) && pwd)
 
+usage(){
+    echo "Usage: ./setup.sh [OPTIONS]"
+    echo "  Options:"
+    echo "      -l  :   Linux"
+    echo "      -m  :   Mac"
+    echo "      -w  :   Windows"
+    exit 1
+}
+
 copy_file(){
     filename=$1
 
@@ -29,7 +38,7 @@ make_directory(){
     fi
 }
 
-main(){
+linux_settings(){
     copy_file .vimrc
     copy_file .zshrc
     copy_file .bashrc
@@ -39,6 +48,51 @@ main(){
 
     make_directory .vim
     copy_directory .vim/colors
+    echo "Linux settings done"
+    exit 0
+}
+
+mac_settings(){
+    ./MacOSX/setup.sh
+    echo "MacOSX settings done"
+    exit 0
+}
+
+cygwin_settings(){
+    copy_file .vimrc
+    copy_file Windows/cygwin/.bashrc
+    copy_file Windows/cygwin/.minttyrc
+
+    copy_directory misc
+
+    make_directory .vim
+    copy_directory .vim/colors
+    echo "Cygwin settings done"
+    exit 0
+}
+
+main(){
+    while getopts lmwh OPT
+    do
+        case $OPT in
+            h)
+                usage
+                ;;
+            u)
+                linux_settings
+                ;;
+            m)
+                mac_settings
+                ;;
+            w)
+                cygwin_settings
+                ;;
+            \?)
+                usage
+                ;;
+        esac
+    done
+    usage
 }
 
 main "$@"
